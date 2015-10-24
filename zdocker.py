c9c7a5ce024891964f0800ace5225c6ad09ec548
aserdyuk <seal@ciklum.com>
2015-10-24 15:30:08 +0300

    return json lis run containers

--- zdocker.py
+++ zdocker.py
@@ -8,9 +8,6 @@ cli = Client(base_url='unix://var/run/docker.sock')
 args = '7e82f243affde6807a44b9c1725abb4da8e1980992fe05c81e48ace10c6be358'
 docker_inspect = cli.inspect_container(args)
 test = cli.containers()
-# print test
-# docker_inspect2 = json.loads(docker_inspect)
-# print docker_inspect
 
 def container_name(cli):
 	count_containers = 0
@@ -19,22 +16,23 @@ def container_name(cli):
 	for container in containers:
 		status = container['Status']
 		if status.startswith("Up"):
-			docker_run_name.append('{{#DOCKER_NAME}:' + str(container['Names']) + '}')
-			# docker_run_name = container['Names']
+			docker_run_name.append(dict([('{{#DOCKER_NAME}', str(container['Names'][0][1:])) ]))
 			docker_run_id = container['Id']
-			# print json.dumps({
-	  #                   "{#DOCKER_NAME}": docker_run_name,    	  
-	  #                   },)
 	return docker_run_name
-
-
 print (json.dumps({'data': (container_name(cli))}, indent=4))
-# print (container_name(cli))
-# print(json.dumps("date": {"c": 0, "b": 0, "a": 0}))
-# print container_name(cli)
-
-# print (json.dumps({'data': ({'bar': 23},{'bar': 55})}))
 
 
 
-# print docker_run_name
+
+
+
+
+
+
+
+
+
+
+
+
+
